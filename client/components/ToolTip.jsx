8c2afea09938602500b95bbbceb984ace34d4d61
Mia <ngocmhuynh5@gmail.com>
2018-08-01 20:35:49 -0700

    :art: hocs folder, PieChart
    added d3renderer/piechart component #2

--- client/components/ToolTip.jsx
+++ client/components/ToolTip.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const { string, object } = PropTypes; 
+const { string, object } = PropTypes;
 
-const ToolTip = ({ style = {}, content }) => {
-  <div className ='tooltip' style={style}>
+const Tooltip = ({ style = {}, content }) => (
+  <div className="tooltip" style={style}>
     {content}
   </div>
-};
+);
 
-ToolTip.PropTypes = {
+Tooltip.propTypes = {
   content: string, 
   style: object,
-}
+};
 
-export default ToolTip;
+export default Tooltip;
