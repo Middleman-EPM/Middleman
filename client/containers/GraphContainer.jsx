039122fb883c82d7efb6b14ea4f77cd9df096b28
Mia <ngocmhuynh5@gmail.com>
2018-08-05 16:44:58 -0700

    :fire: removed tooltip, hocs folder
    added piechart tester d3 v.2 #1

--- client/containers/GraphContainer.jsx
+++ client/containers/GraphContainer.jsx
@@ -1,43 +1,51 @@
-import React, {Component} from 'react';
-import RouteMap from '../components/RouteMap'
-
+import React, { Component } from 'react';
+import RouteMap from '../components/RouteMap';
 
 class GraphContainer extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.onResize = this.onResize.bind(this)
-    this.onHover = this.onHover.bind(this)
-    this.onBrush = this.onBrush.bind(this)
-    this.state = { screenWidth: 1000, screenHeight: 500, hover: "none", brushExtent: [0, 40] }
+    this.onResize = this.onResize.bind(this);
+    this.onHover = this.onHover.bind(this);
+    this.onBrush = this.onBrush.bind(this);
+    this.state = {
+      screenWidth: 1000,
+      screenHeight: 500,
+      hover: "none",
+      brushExtent: [0, 40]
+    };
   }
   onResize() {
-    this.setState({ screenWidth: window.innerWidth, screenHeight: window.innerHeight - 120 })
+    this.setState({
+      screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight - 120
+    });
   }
 
   onHover(d) {
-    this.setState({ hover: d })
-    console.log(d)
+    this.setState({ hover: d });
+    console.log(d);
   }
 
   onBrush(d) {
-    this.setState({ brushExtent: d })
+    this.setState({ brushExtent: d });
   }
 
   componentDidMount() {
-    window.addEventListener('resize', this.onResize, false)
-    this.onResize()
-
+    window.addEventListener("resize", this.onResize, false);
+    this.onResize();
   }
 
   render() {
-    return <div className="graphcontainer">
+    return (
+      <div className="graphcontainer">
         <div>
-          <h1> Routes & Middleware Map</h1>
+          <h1>Routes & Middleware Map</h1>
         </div>
         <div className="routemap">
-          <RouteMap onHover={this.onHover}/>
+          <RouteMap onHover={this.onHover} />
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 export default GraphContainer;
