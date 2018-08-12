2119e6dc60fc43d1aa35e79059d4f77405b910c5
Jung Shin <crnboii@gmail.com>
2018-08-11 18:48:17 -0700

    implemented promise to handle res.locals

--- server/db/controller.js
+++ server/db/controller.js
@@ -1,23 +1,50 @@
 const db = require('./connect');
-
+let hash_id, route_id, middleware_id;
 const controller = {
   addHash: (req, res, next) => {
     const query = 'INSERT INTO hash (api_key) VALUES($1) RETURNING *';
     const values = [`${req.body.hash}`]//TBD;
+
+    new Promise((resolve, reject) => {
+      const query1 = db.query(query, values, (err, result) => {
+        if(err) console.log('Error adding api_key to table')
+        else {
+          res.locals.hash_id = result.rows[0]._id;
+          console.log('this is running in addHash ',result.rows[0]._id);
+            resolve(query1)
+        }
+      })
+
+    }).then(()=> next())
+  },
+  addRoute: (req, res, next) => {
+    console.log('this is add route',res.locals)
+    const query = 'INSERT INTO routes (route, foreign_key) VALUES($1, $2) RETURNING *';
+    const values = [`${req.body.route}`, `${res.locals.hash_id}`]//TBD;
+    // console.log(req.body.route)
+    setTimeout(() => db.query(query, values, (err, result) => {
+      console.log(res.locals)
+      if (err) console.log('Error adding routes to table')
+      else res.locals.route_id = result.rows[0]._id;
+    }), 0)
+    // next();
+  },
+  addMiddleware: (req, res, next) => {
+    const query = 'INSERT INTO middlewares (middleware) VALUES($1) RETURNING *';
+    const values = [`${req.body.middleware}`]//TBD;
     db.query(query, values, (err, result) => {
-      if(err) console.log('Error adding api_key to table')
-      console.log(results.rows[0])
+      if (err) console.log('Error adding api_key to table')
+      else req.body.middleware_id = result.rows[0]._id;
     })
     next();
   },
-  addRoutes: (req, res, next) => {
-
-  },
-  addMiddleware: (req, res, next) => {
-
-  },
   addTimes: (req, res, next) => {
-
+    const query = 'INSERT INTO times (start_time, end_time, duration) VALUES($1, $2, $3) RETURNING *';
+    const values = [`${req.body.times[0]}`, `${req.body.times[1]}`, `${req.body.times[2]}`];//TBD;
+    db.query(query, values, (err, result) => {
+      if (err) console.log('Error adding times to table')
+      else res.send('success!')
+    })
   },
 }
 
